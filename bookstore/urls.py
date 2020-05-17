9e5b43412b9b332b35c63ae45a50c90854774c6b
Pram Sanghani <pramod.s@hotmail.co.uk>
2020-05-17 16:03:43 +0100

    created books app and routing

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
+	path('books/', include('books.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
