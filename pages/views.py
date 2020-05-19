1f94b659da2fbbea9cc9ba805aaadfc7f0fed2a4
Pram Sanghani <pramod.s@hotmail.co.uk>
2020-05-19 15:07:30 +0100

    Bring in data from database to homepage

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from books.models import Books
+from authors.models import Author
 
 # Create homepage
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    books = Books.objects.filter(is_published=True).order_by("-list_date")
+    context = {"books": books}
+    return render(request, "pages/index.html", context)
+
