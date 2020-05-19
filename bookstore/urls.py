95a2671be3bdf86eb20ea0b6d466a04814323408
Pram Sanghani <pramod.s@hotmail.co.uk>
2020-05-19 18:16:21 +0100

    Add accounts url

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', include('pages.urls')),
-    path('admin/', admin.site.urls),
-	path('books/', include('books.urls')),
+    path("", include("pages.urls")),
+    path("admin/", admin.site.urls),
+    path("books/", include("books.urls")),
+    path("accounts/", include("accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
