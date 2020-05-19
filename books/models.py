1ead61823a5ef619cb438681e2fecb1b49fc07a7
Pram Sanghani <pramod.s@hotmail.co.uk>
2020-05-19 14:58:17 +0100

    Created books modules and database

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
+from datetime import datetime
 from django.db import models
+from authors.models import Author
 
 # Create your models here.
+class Books(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category_name = models.CharField(max_length=200)
+    book_image = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
+
--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from .models import Books
 
 # Create your views here.
 def books(request):
     return render(request, "books/books.html")
-
