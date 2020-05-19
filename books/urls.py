bd259e6d6eb6df6823463fb352b3619479e9abe5
Pram Sanghani <pramod.s@hotmail.co.uk>
2020-05-19 16:41:16 +0100

    Single view book page created

--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.books, name="books"),
+    path("book/<int:book_id>", views.book, name="book"),
+    path("search", views.search, name="search"),
 ]
-
