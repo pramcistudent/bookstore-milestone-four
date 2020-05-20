9525d7cdb98eba198952877b8eff1a8f1bf8d860
Pram Sanghani <pramod.s@hotmail.co.uk>
2020-05-20 16:57:01 +0100

    Add message alerts to register & login

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -123,3 +123,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "./static")]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "./media")
+
+# messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {messages.ERROR: "danger"}
