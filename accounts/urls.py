87624198a1c22bb25184c06380f1982f60b800d3
Pram Sanghani <pramod.s@hotmail.co.uk>
2020-05-21 15:55:45 +0100

    Added reset & password change pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -6,4 +8,40 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("logout", views.logout, name="logout"),
     path("profile/", views.profile, name="profile"),
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="registration/password_change.html"
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change_done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="registration/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
+    path(
+        "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+    ),
+    path(
+        "password_reset_done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset_done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
