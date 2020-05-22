824d260275cbd4ac0498c0c9092cc6f131e010f0
Pram Sanghani <pramod.s@hotmail.co.uk>
2020-05-22 18:06:48 +0100

    Created cart views and models

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "cart.context_processors.counter",
             ],
         },
     },
@@ -127,4 +128,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "./media")
 
 # messages
 from django.contrib.messages import constants as messages
+
 MESSAGE_TAGS = {messages.ERROR: "danger"}
