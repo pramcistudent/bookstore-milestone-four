0db3248cc3980770dd16e9032a80081f012624e6
Pram Sanghani <pramod.s@hotmail.co.uk>
2020-06-09 17:01:32 +0100

    Update books url to include add_review path

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from reviews.views import add_review
 
 urlpatterns = [
     path("", views.books, name="books"),
     path("book/<int:book_id>", views.book, name="book"),
     path("search", views.search, name="search"),
+    path("book/<int:pk>/reviews/", add_review, name="add_review"),
 ]
