e00dd48ab4147d2a0becfd0387bced99dd7c9727
Pram Sanghani <pramod.s@hotmail.co.uk>
2020-06-17 15:50:10 +0100

    Update tests for accounts forms.py & views.py

--- accounts/tests_forms.py
+++ accounts/tests_forms.py
@@ -1,45 +1,77 @@
 from django.test import TestCase
 from .forms import LoginForm, RegisterForm
 
+
 # Create your tests here.
 class TestLoginForm(TestCase):
-    def test_user_can_log_in(self):
-        """
-        Tests that the user can login with a username and password
-        """
-        form = LoginForm({"username": "TestUser", "password": "Password"})
-        self.assertTrue(form.is_valid())
 
-    def test_blank_field_error_message(self):
+    def test_login_with_username_only(self):
         """
-        Tests that the fields are required for the form to be valid
+        Tests that the user cannot login with only a username
         """
-        form = LoginForm({"username": ""})
+        form = LoginForm({'username': 'bookStore'})
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors["username"], [u"This field is required."])
+
+    def test_login_with_password_only(self):
+        """
+        Tests that the user cannot login with only a password
+        """
+        form = LoginForm({'password': 'password'})
+        self.assertFalse(form.is_valid())
 
 
 class TestRegisterForm(TestCase):
-    def test_user_can_register(self):
-        """
-        Tests that the user can register by entering the relevant details
-        """
-        form = RegisterForm(
-            {
-                "username": "TestUser",
-                "email": "user@test.com",
-                "first_name": "FirstName",
-                "last_name": "LastName",
-                "password": "Password",
-                "password_confirm": "Password",
-            }
-        )
-        self.assertTrue(form.is_valid())
 
-    def test_blank_field_error_message(self):
+    def test_message_for_missing_firstname(self):
         """
-        Tests that the fields are required for the form to be valid
+        Tests that the correct message is shown when firstname
+        is missing from register form
         """
-        form = RegisterForm({"username": ""})
+        form = RegisterForm({'first_name': ''})
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors["username"], [u"This field is required."])
+        self.assertEqual(form.errors['first_name'], [u'This field is required.'])
+  
+    def test_message_for_missing_lastname(self):
+        """
+        Tests that the correct message is shown when lastname
+        is missing from register form
+        """
+        form = RegisterForm({'last_name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['last_name'], [u'This field is required.'])
+
+    def test_message_for_missing_username(self):
+        """
+        Tests that the correct message is shown when username
+        is missing from register form
+        """
+        form = RegisterForm({'username': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['username'], [u'This field is required.'])
+
+    def test_message_for_missing_email(self):
+        """
+        Tests that the correct message is shown when email
+        is missing from register form
+        """
+        form = RegisterForm({'email': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['email'], [u'This field is required.'])
+
+    def test_message_for_missing_password(self):
+        """
+        Tests that the correct message is shown when password
+        is missing from register form
+        """
+        form = RegisterForm({'password': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['password'], [u'This field is required.'])
+
+    def test_message_for_missing_password_confirm(self):
+        """
+        Tests that the correct message is shown when password reconfirm 
+        is missing from register form
+        """
+        form = RegisterForm({'password_confirm': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['password_confirm'], [u'This field is required.'])
--- accounts/tests_views.py
+++ accounts/tests_views.py
@@ -1,41 +1,59 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
-from django.urls import reverse
-from django.test.client import Client
+from django.contrib.auth.models import User, AnonymousUser
+from django.shortcuts import get_object_or_404
+from accounts.views import register, login, logout, profile
 
 
 # Create your tests here.
-class TestAccountsViews(TestCase):
+class TestView(TestCase):
+
+    def test_get_login_page(self):
+        """
+        Tests that the login page view renders the login template
+        """
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+    def test_can_login(self):
+        """
+        Tests that the user can login using username and passwords
+        """
+        user1 = User.objects.create_user(
+            username='testUser',
+            email='testUser@example.com',
+            password='password')
+
+        response = self.client.get('/books/')
+        self.assertEqual(response.context['user'], AnonymousUser())
+
+        response = self.client.post("/accounts/login", {
+            'username': 'testUser',
+            'password': 'password'
+        })
+
+        response = self.client.get('/accounts/login')
+
+    def test_logout_view(self):
+        """
+        Tests that the user can logout
+        """
+        response = self.client.get("/accounts/logout")
+        self.assertRedirects(response, '/')
+
     def test_get_register_page(self):
         """
         Tests that the register view renders the register template
         """
-        response = self.client.get("/accounts/register")
+        response = self.client.get('/accounts/register')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "accounts/register.html")
+        self.assertTemplateUsed(response, 'accounts/register.html')
 
-    def test_get_login_page(self):
+    def test_get_profile_page(self):
         """
-        Tests that the login view renders the login template
+        Tests that the user can view the profile page when user is logged in
         """
-        response = self.client.get("/accounts/login/")
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "accounts/login.html")
-
-    def setUp(self):
-        """
-        Create test user
-        """
-        self.client = Client()
-        self.user = User.objects.create_user("joe", "joe@bloggs.com", "joepassword")
-
-    def testLogin_get_profile_page(self):
-        """
-        Tests that the profile view redirects to the profile template
-        """
-        self.client.login(username="joe", password="joepassword")
-        response = self.client.get(reverse("index"))
-        response = self.client.get("/accounts/profile/")
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "accounts/profile.html")
-        self.assertEqual(response.status_code, 200)
+        self.client.login(username='profileName', password='profilePassword')
+        response = self.client.get('/accounts/profile/')
+        self.assertEqual(response.status_code, 302)
+        
