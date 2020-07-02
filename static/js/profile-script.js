9e1b65b583f9f27ff3224f80507a93d480819f88
Pram Sanghani <pramod.s@hotmail.co.uk>
2020-07-02 19:56:45 +0000

    Multitab scripts updated for profile.html page

---
+++ static/js/profile-script.js
@@ -0,0 +1,19 @@
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+}
+
+$(document).ready(function() {
+    let pageValue = getUrlParameter('page');
+    if (pageValue) {
+        document.getElementById("history-tab").click();
+    }
+});
--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,34 @@
-/*
-* Confirmation pop up to remove item from cart
-*/ 
-$('.delete').click(function () {
-  return confirm("Are you sure you want to remove the item from cart?");
-});
-
 /* 
 * Multitab function for profile and review page
 */
 function multiTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+
+$(document).ready(function () {
+    /*
+    * Confirmation pop up to remove item from cart
+    */
+    $('.delete').click(function () {
+        return confirm("Are you sure you want to remove the item from cart?");
+    });
+
+    if ($('#message').length > 0) {
+        setTimeout(function () {
+            $('#message').remove();
+        }, 3000);
+    }
+
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+});
